4cd430b00bbc30aa89d3f2d3023b3c7dfffe8180
inamuu <8310973+inamuu@users.noreply.github.com>
2020-11-08 02:07:48 +0900

    Install enpass

--- install.sh
+++ install.sh
@@ -48,3 +48,9 @@ add-apt-repository \
 
 sudo apt install -y docker-ce
 
+# enpass
+echo "deb https://apt.enpass.io/ stable main" > \
+  /etc/apt/sources.list.d/enpass.list
+wget -O - https://apt.enpass.io/keys/enpass-linux.key | apt-key add -
+apt install -y enpass
+
